40f0af563fb79923dca95fc4e69c3085640199a2
ZEE <white.noise616@gmail.com>
2023-04-24 01:15:58 -0600

    feat: added score

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Manager : MonoBehaviour
 {
     public static Manager Instance { get; private set; }
     public int Score { get; private set; }
     public int RivalScore { get; private set; }
+
+    public GameObject UI;
 
     private void Awake()
     {
@@ -29,10 +32,26 @@ public class Manager : MonoBehaviour
     public void AddScore(int num)
     {
         Score += num;
+        UpdateUIScore("Score", "Score:" + Score);
     }
 
     public void AddRivalScore(int num)
     {
         RivalScore += num;
+        UpdateUIScore("RivalScore", "RivalScore:" + RivalScore);
+
+    }
+
+    public void UpdateUIScore(string key, string newVal)
+    {
+        Transform[] children = UI.GetComponentsInChildren<Transform>();
+        foreach (var child in children)
+        {
+            if (child.gameObject.name == key)
+            {
+                child.gameObject.GetComponentInChildren<TMP_Text>().text = newVal;
+            }
+        }
+
     }
 }
