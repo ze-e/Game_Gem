5df1584af74f69d8d4355c0b1e285328f34acc8d
ZEE <white.noise616@gmail.com>
2023-05-08 21:21:52 -0600

    fix: TNT

--- Assets/Scripts/TNT.cs
+++ Assets/Scripts/TNT.cs
@@ -6,41 +6,62 @@ public class TNT : MonoBehaviour
 {
     public float explosionDelay = 3.0f; // the delay in seconds before the TNT explodes
     public GameObject explosionPrefab; // the prefab for the explosion effect
-
-    private bool exploded = false;
-    //throttle
-    int throttle = 0;
-    public int throttleBy = 10000;
+    SpriteRenderer spriteRenderer;
+    Color originalColor;
 
     // Start is called before the first frame update
     void Start()
     {
         // start the countdown to the explosion
         StartCoroutine(ExplodeAfterDelay());
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+        InvokeRepeating("FlashWhite", 0.25f, 0.25f);
     }
 
-    //private void FixedUpdate()
-    //{
-    //    if (throttle % throttleBy == 0)
-    //    {
-    //        Manager.Instance.ReddenSprite(gameObject, throttle, throttleBy);
-    //    }
+    void FlashWhite()
+    {
+        StartCoroutine(FlashWhiteCoroutine());
+    }
 
-    //    throttle++;
+    IEnumerator FlashWhiteCoroutine()
+    {
+        // Set the sprite color to white over 0.1 seconds using Color.Lerp()
+        float t = 0;
+        while (t < 1)
+        {
+            spriteRenderer.color = Color.Lerp(originalColor, Color.grey, t);
+            t += Time.deltaTime / 0.1f;
+            yield return null;
+        }
+
+        // Set the sprite color back to its original color over 0.1 seconds using Color.Lerp()
+        t = 0;
+        while (t < 1)
+        {
+            spriteRenderer.color = Color.Lerp(Color.grey, originalColor, t);
+            t += Time.deltaTime / 0.1f;
+            yield return null;
+        }
+    }
+
+    //private void FlashWhite()
+    //{
+    //    Color color = spriteRenderer.color;
+    //    spriteRenderer.color = Color.grey;
+    //    Invoke("ResetColor", 0.05f);
     //}
 
-
+    //private void ResetColor()
+    //{
+    //    spriteRenderer.color = originalColor;
+    //}
 
     IEnumerator ExplodeAfterDelay()
     {
         // wait for the explosion delay
         yield return new WaitForSeconds(explosionDelay);
-
-        // if the TNT hasn't exploded yet, explode now
-        if (!exploded)
-        {
-            Explode();
-        }
+        Explode();
     }
 
     void Explode()
